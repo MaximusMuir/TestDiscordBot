2fae800b63694aec2b99ee8b455a637dc1078669
MaximusMuir <110356009+MaximusMuir@users.noreply.github.com>
2025-03-10 22:49:38 +0000

    Create keep_alive.py

---
+++ keep_alive.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from threading import Thread
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "I'm alive!"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+# Start Flask server in a separate thread
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
